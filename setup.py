00bcf943615642f827f2628d285d00f7d7e7f75d
aaron-shbrown <aaron.shbrown@gmail.com>
2021-08-29 22:16:39 -0700

    Add files via upload
    Updated setup

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Abrown", # Replace with your own username
+    name="SIPmath", # Replace with your own username
     version="1",
     author="Aaron Brown",
     author_email="aaron@probabilitymanagement.orc",
     description="All the SIPmath related functions",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/probability-management/PyHDR",
+    url="https://github.com/probability-management/PySIPmath",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
