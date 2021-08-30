e111830e83c98869243f8f83c23bd4145e033163
aaron-shbrown <aaron.shbrown@gmail.com>
2021-08-29 22:19:14 -0700

    Add files via upload
    PySIP2 update

--- PySIP2library.py
+++ PySIP2library.py
@@ -126,45 +126,6 @@ def SIP2library(SIPdata, file_name, author, SIPmetadata = []):
     
     workbook.close()
     print("Done! SIP Library saved successfully to your current working directory.")
-
-# Now lets see if our function works!
-# First, create all of the required inputs:
-
-import pandas as pd
-
-# How many trials do you want in your SIP Library?
-N = 11
-
-# Generate a few SIPs:
-SIP_1 = np.random.normal(40, 10, N)
-SIP_2 = np.random.normal(40, 10, N)
-SIP_3 = np.random.normal(40, 10, N)
-SIP_4 = np.random.normal(40, 10, N)
-SIP_5 = np.random.normal(40, 10, N)
-
-# Create a single dataframe containing all of the SIPs:
-Data_Frame = pd.DataFrame({'SIP_1': SIP_1, 'SIP_2': SIP_2,
-                      'SIP_3': SIP_3, 'SIP_4': SIP_4, 'SIP_5': SIP_5})
-
-# Python indexing starts at 0 and we want our index names to start at 1.
-# My function will do this automatically, but for completion, I do it here, too
-Data_Frame.index = {1,2,3,4,5,"...", 996, 997, 998, 999, 1000}
-
-# Create the optional metadate dataframe
-Meta_Data = pd.DataFrame(Data_Frame.describe())
-
-# You can enter the file name as a text string as an argument,
-# But I prefer to use variables whenever possible
-file_name1 = "SIP_Library_no_metadata.xlsx"
-file_name2 = "SIP_Library_with_metadata.xlsx"
-
-# Similarly, you can enter the author name as a text string as an argument,
-# But I prefer to use variables whenever possible
-author = "Shaun Doheney"
-
-# Now that you've created all the required (and optional) inputs,
-# You're ready to test the function.
-# This tests the case where you do not include metadata:
 SIPLibraryGenerator(Data_Frame, file_name1, author)
 
 # This tests the case where you do include metadata:
