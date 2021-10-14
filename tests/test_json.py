45447537c0608b02b5f42ba644d4217ab11a0c41
Gavin Morgan <morgan.gavin@gmail.com>
2021-10-14 12:48:43 -0700

    test that PySIP.Json writes a file

--- tests/test_json.py
+++ tests/test_json.py
@@ -11,8 +11,15 @@ class BasicTestSuite(unittest.TestCase):
     m = mock_open()
 
     @patch('pandas.DataFrame')
+    @patch("builtins.open", m, create=True)
     def test_Json_is_a_function(self, MockDataFrame):
         PySIP.Json(MockDataFrame(), "test-output.json", "bar")
+
+    @patch('pandas.DataFrame')
+    @patch("builtins.open", m, create=True)
+    def test_Json_writes_a_file(self, MockDataFrame):
+        PySIP.Json(MockDataFrame(), "foo.json", "bar")
+        open.assert_called_with("foo.json", "w")
 
 if __name__ == '__main__':
     unittest.main()
