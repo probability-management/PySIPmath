2cd46f9cd4866665c7a9be8e6123bbd592477110
Gavin Morgan <gavin@probabilitymanagement.org>
2021-10-25 14:29:01 -0700

    introduces fit fixture to speed up tests

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from .context import PySIP
+from .fixtures.fit import fit_fixture
 
 import unittest
 from unittest.mock import ANY, MagicMock, mock_open, patch
 from unittest import TestCase
 
 from pandas import DataFrame
-
 
 class BasicTestSuite(unittest.TestCase):
     """Basic test cases."""
@@ -34,9 +34,11 @@ class BasicTestSuite(unittest.TestCase):
             'version': '1'
         }, mock_dump.call_args[0][0])
 
+    @patch("metalog.metalog.fit")
     @patch("json.dump")
     @patch("builtins.open")
-    def test_Json_contains_metadata(self, mock_open, mock_dump):
+    def test_Json_contains_metadata(self, mock_open, mock_dump, mock_fit):
+        mock_fit.return_value = fit_fixture()
         self.maxDiff = 800
         fixture = DataFrame(data={
             "Accounts": [10.24313638, 13.69812026, 12.62841292, 2.890162231, 7.60269451],
@@ -55,41 +57,45 @@ class BasicTestSuite(unittest.TestCase):
             'std': 4.336325622060243
         }, mock_dump.call_args[0][0]["sips"][0]["metadata"])
 
+    @patch("metalog.metalog.fit")
     @patch("json.dump")
     @patch("builtins.open")
-    def test_Json_contains_density_metadata(self, mock_open, mock_dump):
-        self.maxDiff = 800
+    def test_Json_contains_density_metadata(self, mock_open, mock_dump, mock_fit):
+        mock_fit.return_value = fit_fixture()
+        self.maxDiff = None
         fixture = DataFrame(data={
             "Accounts": [10.24313638, 13.69812026, 12.62841292, 2.890162231, 7.60269451],
             "Products": [7.00895936, 11.61220758, 5.07099725, 7.542072262, 13.37670202],
         })
         PySIP.Json(fixture, "foo.json", "bar")
+        # These numbers are derived from the `fit_fixture`, and differ from those derived
+        # from the original metalog.metalog.fit, somehow.
         self.assertListEqual([
-            999.9999999999998,
-            1000.0,
-            1000.0,
-            1000.0,
-            1000.0,
-            1000.0,
-            1000.0,
-            1000.0,
-            1000.0,
-            1000.0,
-            1000.0,
-            1000.0,
-            1000.0,
-            1000.0,
-            1000.0,
-            1000.0,
-            1000.0,
-            1000.0,
-            1000.0,
-            1000.0,
-            1000.0,
-            1000.0,
-            1000.0,
-            1000.0,
-            999.9999999999998
+            0.0025771154162709774,
+            0.07982657640547815,
+            0.029550125014221145,
+            0.017235957098999936,
+            0.011535801921317236,
+            0.008261040431818098,
+            0.006143783605397077,
+            0.004674061970193781,
+            0.003647715373304234,
+            0.002885727261202881,
+            0.0022485786991766413,
+            0.0018531373331520753,
+            0.0015078193031848432,
+            0.001162501273217611,
+            0.0009057936754070011,
+            0.0007265846238809351,
+            0.000586335727297715,
+            0.0004729949810635803,
+            0.0003819172217154018,
+            0.00030610153965857025,
+            0.000250857227215982,
+            0.0002006087608621535,
+            0.00016834437845785648,
+            0.00013607999605355945,
+            0.00010381561364926245
         ], mock_dump.call_args[0][0]["sips"][0]["metadata"]["density"])
 
 
